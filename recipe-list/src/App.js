ab5460e746f8ca16928c64041c46f0c069676545
Amey Sawant <mailforamey@gmail.com>
2021-06-19 22:48:33 +0530

    first commit

---
+++ recipe-list/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Dish from "./components/Dish";
+
+const App = () => {
+  return (
+    <>
+      <Dish />
+      <Dish />
+      <Dish />
+    </>
+  );
+};
+
+export default App;
---
+++ recipe-list/src/components/Dish.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Dish = () => {
+  return (
+    <div className="column">
+      <img
+        className="image"
+        src="https://firebasestorage.googleapis.com/v0/b/ruchika-rasoi.appspot.com/o/images%2Fproducts%2F3-12-23-chicken-boneless-biryani.jpg?alt=media&token=1cb41302-82cb-4e0f-9ac3-32e008e06542"
+        alt=""
+      />
+      <h4 className="heading">Chicken Boneless Biryani</h4>
+      <p className="price">Rs. 325/-</p>
+    </div>
+  );
+};
+
+export default Dish;
